f05a70ec4246776a7d8cc0b79e82d5c1f07192ee
Fire-orel <78948895+Fire-orel@users.noreply.github.com>
2023-05-27 18:46:39 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import wget
+from telebot import TeleBot
+import os.path
+import datetime
+
+
+api_token="6232828877:AAEk1NvcISCrLcVdWWxtllSNGw_UdFdmHrQ"
+bot=TeleBot(api_token)
+
+@bot.message_handler(commands=["help","start"])
+def send_welcome(message):
+    bot.send_message(message.chat.id,"Здравствуйте я бот для скачивания презентаций по ссылке")
+
+@bot.message_handler(content_types="text")
+def send_prezintacia(message):
+    if message.text.split('https://', maxsplit = 1)[0] == '' or message.text.split('http://', maxsplit = 1)[0] == '':
+        try:
+            bot.send_message(message.chat.id,"Файл скачиваеться ожидайте...")
+            url = message.text
+            wget.download(url, f'{message.from_user.id}.pptx')
+            f=open(f'{message.from_user.id}.pptx',"rb")
+            bot.send_document(message.chat.id,f)
+            f.close()
+            os.remove(f'{message.from_user.id}.pptx')
+            with open('history.txt', 'a') as h:
+                h.write(f'{datetime.datetime.now()}, {message.chat.username} \n')
+            h.close()
+        except :
+            bot.send_message(message.chat.id,"Непредвиденная ошибка попробуйте ещё раз")
+    else:
+        bot.send_message(message.chat.id,"Формат ссылки не правильный")
+
+
+
+bot.polling(none_stop=True)
+
+
+
